b0b7aae5d1b5a333e20e3bee44dc8523ee2a7a45
johnkennedyb <johnkennedy3313@gmail..com>
2025-09-16 17:48:27 +0100

    commit

--- server.js
+++ server.js
@@ -119,6 +119,10 @@ app.use((err, req, res, next) => {
   });
 });
 
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to Apparcus API!' });
+});
+
 // 404 handler
 app.use('*', (req, res) => {
   res.status(404).json({ message: 'Route not found' });

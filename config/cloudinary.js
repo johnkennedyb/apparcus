1ccf752e22acf2db3fd0c903b2bbc8bc7e85eab5
johnkennedyb <johnkennedy3313@gmail..com>
2025-09-18 16:50:14 +0100

    po

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -3,7 +3,7 @@ import { v2 as cloudinary } from 'cloudinary';
 // Configure Cloudinary with your credentials
 cloudinary.config({
   cloud_name:'dw7w2at8k', // You'll need to provide the correct cloud name
-  api_key: '268257791245894',
+  api_key: '429854431267325',
   api_secret: 'Zw3fw4RaORLS4f9HldknHfqb7yE'
 });
 
